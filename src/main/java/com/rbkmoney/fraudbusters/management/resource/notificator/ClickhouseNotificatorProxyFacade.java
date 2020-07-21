aa6569d77c5ceaadec68de6074d2c50e390514ac
k.struzhkin <k.struzhkin@MacBook-Pro-rbkadmin.local>
2020-07-21 18:31:33 +0300

    Fix tests

--- src/main/java/com/rbkmoney/fraudbusters/management/resource/notificator/ClickhouseNotificatorProxyFacade.java
+++ src/main/java/com/rbkmoney/fraudbusters/management/resource/notificator/ClickhouseNotificatorProxyFacade.java
@@ -5,6 +5,7 @@ import com.rbkmoney.fraudbusters.management.resource.notificator.domain.Notifica
 import com.rbkmoney.fraudbusters.management.resource.notificator.domain.ValidationResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -15,37 +16,40 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class ClickhouseNotificatorProxyFacade {
 
+    @Value("${ch.notificator.url}")
+    private String baseUrl;
+
     private final RestTemplate restTemplate;
 
     @PostMapping(value = "/notification")
     public Notification createOrUpdate(@Validated @RequestBody Notification notification) {
-        ResponseEntity<Notification> notificationResponseEntity = restTemplate.postForEntity("/ch-manager/notification", notification, Notification.class);
+        ResponseEntity<Notification> notificationResponseEntity = restTemplate.postForEntity(baseUrl + "/notification", notification, Notification.class);
         log.info("ClickhouseNotificatorFacade created notification: {}", notification);
         return notificationResponseEntity.getBody();
     }
 
     @DeleteMapping(value = "/notification/{name}")
     public void delete(@Validated @PathVariable String name) {
-        restTemplate.delete("/ch-manager/notification/{name}", name);
+        restTemplate.delete(baseUrl + "/notification/{name}", name);
         log.info("ClickhouseNotificatorFacade deleted name: {}", name);
     }
 
     @PostMapping(value = "/notification/validate")
     public ValidationResponse validate(@Validated @RequestBody Notification notification) {
-        ResponseEntity<ValidationResponse> responseEntity = restTemplate.postForEntity("/ch-manager/notification",
+        ResponseEntity<ValidationResponse> responseEntity = restTemplate.postForEntity(baseUrl + "/notification",
                 notification, ValidationResponse.class);
         return responseEntity.getBody();
     }
 
     @PostMapping(value = "/channel")
     public Channel createOrUpdate(@Validated @RequestBody Channel channel) {
-        ResponseEntity<Channel> responseEntity = restTemplate.postForEntity("/ch-manager/channel", channel, Channel.class);
+        ResponseEntity<Channel> responseEntity = restTemplate.postForEntity(baseUrl + "/channel", channel, Channel.class);
         return responseEntity.getBody();
     }
 
     @DeleteMapping(value = "/channel/{name}")
     public void deleteChannel(@Validated @PathVariable String name) {
-        restTemplate.delete("/ch-manager/channel/{name}", name);
+        restTemplate.delete(baseUrl + "/channel/{name}", name);
         log.info("ClickhouseNotificatorFacade deleted name: {}", name);
     }
 
