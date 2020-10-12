872353026c56c74c7ddf019f432bd5c4fed0e25b
NPospolita <nikita7asics@gmail.com>
2020-10-12 11:17:38 +0300

    other port

--- src/main/java/com/rbkmoney/fraudbusters/management/config/SecurityConfig.java
+++ src/main/java/com/rbkmoney/fraudbusters/management/config/SecurityConfig.java
@@ -13,7 +13,7 @@ public class SecurityConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
             }
         };
     }
