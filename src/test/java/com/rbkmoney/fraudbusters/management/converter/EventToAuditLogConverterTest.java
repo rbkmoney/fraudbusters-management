0afdcf766e276089b08da9bacd67c40d94a2f9c9
k.struzhkin <koztyastruga@gmail.com>
2021-01-14 13:41:51 +0300

    Fix log lists

---
+++ src/test/java/com/rbkmoney/fraudbusters/management/converter/EventToAuditLogConverterTest.java
@@ -0,0 +1,38 @@
+package com.rbkmoney.fraudbusters.management.converter;
+
+import com.rbkmoney.damsel.wb_list.*;
+import com.rbkmoney.fraudbusters.management.domain.tables.pojos.CommandAudit;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.StringUtils;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class EventToAuditLogConverterTest {
+
+    private final EventToAuditLogConverter eventToAuditLogConverter = new EventToAuditLogConverter();
+
+    @Test
+    void convert() {
+        CommandAudit audit = eventToAuditLogConverter.convert(new Event().setEventType(EventType.CREATED)
+                .setRow(new Row()
+                        .setId(IdInfo.p2p_id(new P2pId()))
+                        .setListName("list")
+                        .setListType(ListType.black)
+                )
+                .setUserInfo(new UserInfo())
+                .setCommandTime(""));
+
+        assertFalse(StringUtils.isEmpty(audit.getObject()));
+
+        audit = eventToAuditLogConverter.convert(new Event().setEventType(EventType.CREATED)
+                .setRow(new Row()
+                        .setId(IdInfo.payment_id(new PaymentId()))
+                        .setListName("list")
+                        .setListType(ListType.black)
+                )
+                .setUserInfo(new UserInfo())
+                .setCommandTime(""));
+
+        assertFalse(StringUtils.isEmpty(audit.getObject()));
+    }
+}
