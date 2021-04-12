293293794cbb9011310bf5afaadb093b28807e63
Kostya <koztyastruga@gmail.com>
2021-04-12 18:08:48 +0300

    Fix default (#79)

--- src/main/java/com/rbkmoney/fraudbusters/management/dao/payment/reference/PaymentReferenceDao.java
+++ src/main/java/com/rbkmoney/fraudbusters/management/dao/payment/reference/PaymentReferenceDao.java
@@ -16,4 +16,6 @@ public interface PaymentReferenceDao extends ReferenceDao<PaymentReferenceModel>
 
     Integer countFilterModel(String searchValue);
 
+    Boolean isReferenceExistForPartyAndShop(String partyId, String shopId);
+
 }
