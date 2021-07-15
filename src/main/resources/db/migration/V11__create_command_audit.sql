ff93e3d9a086919ef2bc2f28867db691bc7eb36d
k.struzhkin <koztyastruga@gmail.com>
2021-07-15 17:10:09 +0300

    Revert tables

--- src/main/resources/db/migration/V11__create_command_audit.sql
+++ src/main/resources/db/migration/V11__create_command_audit.sql
@@ -4,11 +4,11 @@ CREATE TYPE af.command_type AS ENUM('CREATE', 'DELETE');
 
 -- af.command_audit --
 CREATE TABLE af.command_audit(
-                                 id                   BIGSERIAL         NOT NULL,
-                                 initiator            CHARACTER VARYING NOT NULL,
-                                 object_type          af.object_type       NOT NULL,
-                                 command_type         af.command_type      NOT NULL,
-                                 object               CHARACTER VARYING NOT NULL,
-                                 insert_time          TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() at time zone 'utc'),
-                                 CONSTRAINT command_audit_pkey PRIMARY KEY (id)
+  id                   BIGSERIAL         NOT NULL,
+  initiator            CHARACTER VARYING NOT NULL,
+  object_type          af.object_type       NOT NULL,
+  command_type         af.command_type      NOT NULL,
+  object               CHARACTER VARYING NOT NULL,
+  insert_time          TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() at time zone 'utc'),
+  CONSTRAINT command_audit_pkey PRIMARY KEY (id)
 );
