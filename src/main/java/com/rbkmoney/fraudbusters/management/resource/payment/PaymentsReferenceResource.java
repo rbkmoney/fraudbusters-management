05c1c6cb3642a120ccec84939a9e8a845cda35e8
k.struzhkin <koztyastruga@gmail.com>
2021-07-19 16:29:23 +0300

    Fix tests

--- src/main/java/com/rbkmoney/fraudbusters/management/resource/payment/PaymentsReferenceResource.java
+++ src/main/java/com/rbkmoney/fraudbusters/management/resource/payment/PaymentsReferenceResource.java
@@ -1,18 +1,19 @@
 package com.rbkmoney.fraudbusters.management.resource.payment;
 
-import com.rbkmoney.damsel.fraudbusters.CommandType;
 import com.rbkmoney.fraudbusters.management.converter.payment.DefaultPaymentReferenceModelToPaymentReferenceConverter;
-import com.rbkmoney.fraudbusters.management.converter.payment.PaymentReferenceModelToCommandConverter;
 import com.rbkmoney.fraudbusters.management.converter.payment.PaymentReferenceModelToPaymentReferenceConverter;
-import com.rbkmoney.fraudbusters.management.converter.payment.ReferenceToCommandConverter;
 import com.rbkmoney.fraudbusters.management.dao.payment.DefaultPaymentReferenceDaoImpl;
 import com.rbkmoney.fraudbusters.management.dao.payment.reference.PaymentReferenceDao;
 import com.rbkmoney.fraudbusters.management.domain.payment.DefaultPaymentReferenceModel;
 import com.rbkmoney.fraudbusters.management.domain.payment.PaymentReferenceModel;
 import com.rbkmoney.fraudbusters.management.domain.request.FilterRequest;
 import com.rbkmoney.fraudbusters.management.filter.UnknownPaymentTemplateInReferenceFilter;
-import com.rbkmoney.fraudbusters.management.service.payment.PaymentTemplateReferenceService;
-import com.rbkmoney.fraudbusters.management.utils.*;
+import com.rbkmoney.fraudbusters.management.service.payment.PaymentsDefaultReferenceService;
+import com.rbkmoney.fraudbusters.management.service.payment.PaymentsReferenceService;
+import com.rbkmoney.fraudbusters.management.utils.FilterRequestUtils;
+import com.rbkmoney.fraudbusters.management.utils.PagingDataUtils;
+import com.rbkmoney.fraudbusters.management.utils.PaymentUnknownTemplateFinder;
+import com.rbkmoney.fraudbusters.management.utils.UserInfoService;
 import com.rbkmoney.swag.fraudbusters.management.api.PaymentsReferencesApi;
 import com.rbkmoney.swag.fraudbusters.management.model.PaymentReference;
 import com.rbkmoney.swag.fraudbusters.management.model.ReferencesResponse;
@@ -27,7 +28,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -36,16 +36,14 @@ import java.util.stream.Collectors;
 public class PaymentsReferenceResource implements PaymentsReferencesApi {
 
     private final PaymentReferenceDao referenceDao;
-    private final DefaultPaymentReferenceDaoImpl defaultPaymentReferenceDao;
     private final UserInfoService userInfoService;
-    private final PaymentUnknownTemplateFinder unknownTemplateFinder;
-    private final ReferenceToCommandConverter referenceToCommandConverter;
-    private final UnknownPaymentTemplateInReferenceFilter templateInReferenceFilter;
-    private final CommandMapper commandMapper;
-    private final PaymentTemplateReferenceService paymentTemplateReferenceService;
-    private final PaymentReferenceModelToCommandConverter paymentReferenceModelToCommandConverter;
     private final PaymentReferenceModelToPaymentReferenceConverter modelToPaymentReferenceConverter;
+    private final PaymentsReferenceService paymentsReferenceService;
+    private final DefaultPaymentReferenceDaoImpl defaultPaymentReferenceDao;
+    private final PaymentsDefaultReferenceService paymentsDefaultReferenceService;
     private final DefaultPaymentReferenceModelToPaymentReferenceConverter defaultModelToPaymentReferenceConverter;
+    private final PaymentUnknownTemplateFinder unknownTemplateFinder;
+    private final UnknownPaymentTemplateInReferenceFilter templateInReferenceFilter;
 
     @Override
     @PreAuthorize("hasAnyRole('fraud-officer')")
@@ -69,6 +67,24 @@ public class PaymentsReferenceResource implements PaymentsReferencesApi {
 
     @Override
     @PreAuthorize("hasAnyRole('fraud-officer')")
+    public ResponseEntity<String> insertDefaultReference(@Valid PaymentReference paymentReference) {
+        log.info("insertDefaultReference initiator: {} referenceModels: {}", userInfoService.getUserName(),
+                paymentReference);
+        String uid = paymentsDefaultReferenceService.inertDefaultReference(paymentReference);
+        return ResponseEntity.ok().body(uid);
+    }
+
+
+    @Override
+    @PreAuthorize("hasAnyRole('fraud-officer')")
+    public ResponseEntity<String> removeDefaultReference(String id) {
+        log.info("removeDefaultReference initiator: {} id: {}", userInfoService.getUserName(), id);
+        defaultPaymentReferenceDao.remove(id);
+        return ResponseEntity.ok().body(id);
+    }
+
+    @Override
+    @PreAuthorize("hasAnyRole('fraud-officer')")
     public ResponseEntity<ReferencesResponse> filterReferences(@Valid String lastId, @Valid String sortOrder,
                                                                @Valid String searchValue, @Valid String sortBy,
                                                                @Valid String sortFieldValue, @Valid Integer size) {
@@ -86,24 +102,6 @@ public class PaymentsReferenceResource implements PaymentsReferencesApi {
 
     @Override
     @PreAuthorize("hasAnyRole('fraud-officer')")
-    public ResponseEntity<String> insertDefaultReference(@Valid PaymentReference paymentReference) {
-        log.info("insertDefaultReference initiator: {} referenceModels: {}", userInfoService.getUserName(),
-                paymentReference);
-        var uid = UUID.randomUUID().toString();
-        var defaultReferenceModel = DefaultPaymentReferenceModel.builder()
-                .id(uid)
-                .lastUpdateDate(paymentReference.getLastUpdateDate().format(DateTimeUtils.DATE_TIME_FORMATTER))
-                .modifiedByUser(userInfoService.getUserName())
-                .partyId(paymentReference.getPartyId())
-                .shopId(paymentReference.getShopId())
-                .templateId(paymentReference.getTemplateId())
-                .build();
-        defaultPaymentReferenceDao.insert(defaultReferenceModel);
-        return ResponseEntity.ok().body(uid);
-    }
-
-    @Override
-    @PreAuthorize("hasAnyRole('fraud-officer')")
     public ResponseEntity<List<String>> insertReferences(@Valid List<PaymentReference> paymentReference) {
         String userName = userInfoService.getUserName();
         log.info("insertReference initiator: {} referenceModels: {}", userName, paymentReference);
@@ -112,20 +110,8 @@ public class PaymentsReferenceResource implements PaymentsReferencesApi {
         if (!CollectionUtils.isEmpty(unknownTemplates)) {
             return new ResponseEntity<>(unknownTemplates, HttpStatus.UNPROCESSABLE_ENTITY);
         }
-        List<String> ids = paymentReference.stream()
-                .map(referenceToCommandConverter::convert)
-                .map(command -> commandMapper.mapToConcreteCommand(userName, command, CommandType.CREATE))
-                .map(paymentTemplateReferenceService::sendCommandSync)
-                .collect(Collectors.toList());
+        List<String> ids = paymentsReferenceService.insertReferences(paymentReference, userName);
         return ResponseEntity.ok().body(ids);
-    }
-
-    @Override
-    @PreAuthorize("hasAnyRole('fraud-officer')")
-    public ResponseEntity<String> removeDefaultReference(String id) {
-        log.info("removeDefaultReference initiator: {} id: {}", userInfoService.getUserName(), id);
-        defaultPaymentReferenceDao.remove(id);
-        return ResponseEntity.ok().body(id);
     }
 
     @Override
@@ -133,10 +119,7 @@ public class PaymentsReferenceResource implements PaymentsReferencesApi {
     public ResponseEntity<String> removeReference(String id) {
         String userName = userInfoService.getUserName();
         log.info("removeReference initiator: {} id: {}", userName, id);
-        PaymentReferenceModel reference = referenceDao.getById(id);
-        var command = paymentReferenceModelToCommandConverter.convert(reference);
-        String commandSendDeletedId = paymentTemplateReferenceService
-                .sendCommandSync(commandMapper.mapToConcreteCommand(userName, command, CommandType.DELETE));
+        String commandSendDeletedId = paymentsReferenceService.removeReference(id, userName);
         return ResponseEntity.ok().body(commandSendDeletedId);
     }
 
