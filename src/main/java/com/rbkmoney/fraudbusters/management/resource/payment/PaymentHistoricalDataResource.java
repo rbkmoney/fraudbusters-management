73c5e5fb7916126faa38f22ef246e427577f71f8
k.struzhkin <koztyastruga@gmail.com>
2021-07-27 12:21:08 +0300

    Fix swag version

--- src/main/java/com/rbkmoney/fraudbusters/management/resource/payment/PaymentHistoricalDataResource.java
+++ src/main/java/com/rbkmoney/fraudbusters/management/resource/payment/PaymentHistoricalDataResource.java
@@ -1,5 +1,8 @@
 package com.rbkmoney.fraudbusters.management.resource.payment;
 
+import com.rbkmoney.damsel.base.BoundType;
+import com.rbkmoney.damsel.base.TimestampInterval;
+import com.rbkmoney.damsel.base.TimestampIntervalBound;
 import com.rbkmoney.damsel.fraudbusters.*;
 import com.rbkmoney.fraudbusters.management.converter.payment.PaymentInfoResultToPaymentsConverter;
 import com.rbkmoney.swag.fraudbusters.management.api.PaymentsHistoricalDataApi;
@@ -12,6 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 @Slf4j
 @RestController
@@ -24,21 +28,23 @@ public class PaymentHistoricalDataResource implements PaymentsHistoricalDataApi 
     @SneakyThrows
     @Override
     @PreAuthorize("hasAnyRole('fraud-officer')")
-    public ResponseEntity<PaymentResponse> filterPaymentsInfo(@Valid String lastId, @Valid String sortOrder,
-                                                              @Valid String searchValue, @Valid String sortBy,
-                                                              @Valid String sortFieldValue, @Valid Integer size,
-                                                              @Valid String partyId, @Valid String shopId,
-                                                              @Valid String paymentId, @Valid String status,
-                                                              @Valid String email, @Valid String providerCountry,
-                                                              @Valid String cardToken, @Valid String fingerprint,
-                                                              @Valid String terminal) {
+    public ResponseEntity<PaymentResponse> filterPayments(@NotNull @Valid String from, @NotNull @Valid String to,
+                                                          @Valid String lastId, @Valid String sortOrder,
+                                                          @Valid String searchValue, @Valid String sortBy,
+                                                          @Valid String sortFieldValue, @Valid Integer size,
+                                                          @Valid String partyId, @Valid String shopId,
+                                                          @Valid String paymentId, @Valid String status,
+                                                          @Valid String email, @Valid String providerCountry,
+                                                          @Valid String cardToken, @Valid String fingerprint,
+                                                          @Valid String terminal, @Valid String invoiceId,
+                                                          @Valid String maskedPan) {
         log.info("-> filterPaymentsInfo lastId: {} size: {} partyId: {} shopId: {} paymentId: {} status: {} email: {}" +
                         " providerCountry: {} cardToken: {} fingerprint: {} terminal: {}",
                 lastId, size, partyId, shopId, paymentId, status, email, providerCountry, cardToken, fingerprint,
                 terminal);
         var payments = historicalDataServiceSrv.getPayments(
-                createFilter(partyId, shopId, paymentId, status, email, providerCountry, cardToken, fingerprint,
-                        terminal),
+                createFilter(createTimestampInterval(from, to), partyId, shopId, paymentId, status, email,
+                        providerCountry, cardToken, fingerprint, terminal),
                 createPage(lastId, size),
                 createPage(sortOrder, sortBy));
         var paymentResponse = new PaymentResponse()
@@ -46,6 +52,16 @@ public class PaymentHistoricalDataResource implements PaymentsHistoricalDataApi 
                 .continuationId(payments.getContinuationId());
         log.info("<- filterPaymentsInfo paymentResponse: {}", paymentResponse);
         return ResponseEntity.ok(paymentResponse);
+    }
+
+    private TimestampInterval createTimestampInterval(String from, String to) {
+        return new TimestampInterval()
+                .setLowerBound(new TimestampIntervalBound()
+                        .setBoundTime(from)
+                        .setBoundType(BoundType.inclusive))
+                .setUpperBound(new TimestampIntervalBound()
+                        .setBoundTime(to)
+                        .setBoundType(BoundType.inclusive));
     }
 
     private Sort createPage(String sortOrder, String sortBy) {
@@ -60,9 +76,11 @@ public class PaymentHistoricalDataResource implements PaymentsHistoricalDataApi 
                 .setSize(size);
     }
 
-    private Filter createFilter(String partyId, String shopId, String paymentId, String status, String email,
-                                String providerCountry, String cardToken, String fingerprint, String terminal) {
+    private Filter createFilter(TimestampInterval timestampInterval, String partyId, String shopId, String paymentId,
+                                String status, String email, String providerCountry, String cardToken,
+                                String fingerprint, String terminal) {
         return new Filter()
+                .setInterval(timestampInterval)
                 .setEmail(email)
                 .setCardToken(cardToken)
                 .setPaymentId(paymentId)
