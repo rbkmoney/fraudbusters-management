a3708dd15cc1cdf2fda568df65ca6b478b6fe665
k.struzhkin <koztyastruga@gmail.com>
2021-07-29 13:27:50 +0300

    Fix api

--- src/main/java/com/rbkmoney/fraudbusters/management/resource/payment/PaymentHistoricalDataResource.java
+++ src/main/java/com/rbkmoney/fraudbusters/management/resource/payment/PaymentHistoricalDataResource.java
@@ -27,25 +27,25 @@ public class PaymentHistoricalDataResource implements PaymentsHistoricalDataApi 
 
     @SneakyThrows
     @Override
-    @PreAuthorize("hasAnyRole('fraud-officer')")
+    @PreAuthorize("hasAnyRole('fraud-support', 'fraud-monitoring', 'fraud-officer')")
     public ResponseEntity<PaymentResponse> filterPayments(@NotNull @Valid String from, @NotNull @Valid String to,
-                                                          @Valid String lastId, @Valid String sortOrder,
-                                                          @Valid String searchValue, @Valid String sortBy,
-                                                          @Valid String sortFieldValue, @Valid Integer size,
+                                                          @Valid String continuationId, @Valid String sortOrder,
+                                                          @Valid String sortBy, @Valid Integer size,
                                                           @Valid String partyId, @Valid String shopId,
                                                           @Valid String paymentId, @Valid String status,
                                                           @Valid String email, @Valid String providerCountry,
                                                           @Valid String cardToken, @Valid String fingerprint,
                                                           @Valid String terminal, @Valid String invoiceId,
                                                           @Valid String maskedPan) {
-        log.info("-> filterPaymentsInfo lastId: {} size: {} partyId: {} shopId: {} paymentId: {} status: {} email: {}" +
-                        " providerCountry: {} cardToken: {} fingerprint: {} terminal: {}",
-                lastId, size, partyId, shopId, paymentId, status, email, providerCountry, cardToken, fingerprint,
+        log.info("-> filterPaymentsInfo continuationId: {} size: {} partyId: {} shopId: {} paymentId: {} status: {} " +
+                        "email: {} providerCountry: {} cardToken: {} fingerprint: {} terminal: {}",
+                continuationId, size, partyId, shopId, paymentId, status, email, providerCountry, cardToken,
+                fingerprint,
                 terminal);
         var payments = historicalDataServiceSrv.getPayments(
                 createFilter(createTimestampInterval(from, to), partyId, shopId, paymentId, status, email,
                         providerCountry, cardToken, fingerprint, terminal),
-                createPage(lastId, size),
+                createPage(continuationId, size),
                 createPage(sortOrder, sortBy));
         var paymentResponse = new PaymentResponse()
                 .result(paymentInfoResultToPaymentsConverter.convert(payments.getData().getPayments()))
