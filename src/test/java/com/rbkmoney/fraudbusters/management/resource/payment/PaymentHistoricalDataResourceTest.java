159a9a4c5e80939b94714c844c390fe9de67623d
k.struzhkin <koztyastruga@gmail.com>
2021-08-02 16:55:11 +0300

    Add tests

--- src/test/java/com/rbkmoney/fraudbusters/management/resource/payment/PaymentHistoricalDataResourceTest.java
+++ src/test/java/com/rbkmoney/fraudbusters/management/resource/payment/PaymentHistoricalDataResourceTest.java
@@ -1,8 +1,7 @@
 package com.rbkmoney.fraudbusters.management.resource.payment;
 
-import com.rbkmoney.damsel.domain.*;
-import com.rbkmoney.damsel.fraudbusters.ClientInfo;
-import com.rbkmoney.damsel.fraudbusters.*;
+import com.rbkmoney.damsel.fraudbusters.HistoricalDataResponse;
+import com.rbkmoney.damsel.fraudbusters.HistoricalDataServiceSrv;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
@@ -16,8 +15,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 
-import java.util.List;
-
+import static com.rbkmoney.fraudbusters.management.resource.utils.ExternalModelBeanFactory.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -41,89 +39,121 @@ public class PaymentHistoricalDataResourceTest {
     public void filterPaymentsInfo() throws Exception {
         when(iface.getPayments(any(), any(), any())).thenReturn(new HistoricalDataResponse()
                 .setData(createHistoricalData()));
-        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("paymentId", "test");
-        params.add("size", "100");
-        params.add("from", "2021-07-27 00:00:00");
-        params.add("to", "2021-07-27 13:28:54");
+        LinkedMultiValueMap<String, String> params = createParams();
         this.mockMvc.perform(get("/payments-historical-data/payments-info")
                 .queryParams(params))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content()
-                        .json("{\n" +
-                                "\t\"continuationId\": null,\n" +
-                                "\t\"result\": [{\n" +
-                                "\t\t\"id\": \"test\",\n" +
-                                "\t\t\"eventTime\": \"2021-07-29T13:16:18.348795\",\n" +
-                                "\t\t\"merchantInfo\": {\n" +
-                                "\t\t\t\"partyId\": \"party\",\n" +
-                                "\t\t\t\"shopId\": \"shop\"\n" +
-                                "\t\t},\n" +
-                                "\t\t\"amount\": 123,\n" +
-                                "\t\t\"currency\": \"RUB\",\n" +
-                                "\t\t\"cardToken\": null,\n" +
-                                "\t\t\"clientInfo\": {\n" +
-                                "\t\t\t\"ip\": \"123.123.123.123\",\n" +
-                                "\t\t\t\"fingerprint\": \"finger\",\n" +
-                                "\t\t\t\"email\": \"email\"\n" +
-                                "\t\t},\n" +
-                                "\t\t\"status\": \"captured\",\n" +
-                                "\t\t\"payerType\": null,\n" +
-                                "\t\t\"mobile\": null,\n" +
-                                "\t\t\"recurrent\": null,\n" +
-                                "\t\t\"error\": {\n" +
-                                "\t\t\t\"errorCode\": null,\n" +
-                                "\t\t\t\"errorReason\": null\n" +
-                                "\t\t},\n" +
-                                "\t\t\"paymentSystem\": \"visa\",\n" +
-                                "\t\t\"paymentCountry\": null,\n" +
-                                "\t\t\"paymentTool\": \"BankCard(token:null, " +
-                                "payment_system:PaymentSystemRef(id:visa), " +
-                                "bin:1234, last_digits:null, bank_name:test)\",\n" +
-                                "\t\t\"provider\": {\n" +
-                                "\t\t\t\"providerId\": \"test\",\n" +
-                                "\t\t\t\"terminalId\": \"1234\",\n" +
-                                "\t\t\t\"country\": \"RUS\"\n" +
-                                "\t\t}\n" +
-                                "\t}]\n" +
-                                "}"));
+                        .json("{\"continuationId\":null,\"result\":[{\"id\":\"test\"," +
+                                "\"eventTime\":\"2021-07-29T13:16:18.348795\",\"merchantInfo\":" +
+                                "{\"partyId\":\"party\",\"shopId\":\"shop\"},\"amount\":123,\"currency\":\"RUB\"," +
+                                "\"cardToken\":null,\"clientInfo\":{\"ip\":\"123.123.123.123\"," +
+                                "\"fingerprint\":\"finger\",\"email\":\"email\"},\"status\":\"captured\"," +
+                                "\"payerType\":null,\"mobile\":null,\"recurrent\":null,\"error\":{\"errorCode\":null," +
+                                "\"errorReason\":null},\"paymentSystem\":\"visa\",\"paymentCountry\":null," +
+                                "\"paymentTool\":\"BankCard(token:null, payment_system:PaymentSystemRef(id:visa)," +
+                                " bin:1234, last_digits:null, bank_name:test)\",\"provider\":{\"providerId\":\"test\"," +
+                                "\"terminalId\":\"1234\",\"country\":\"RUS\"}}]}"));
+    }
+
+    @Test
+    public void filterRefundsInfo() throws Exception {
+        when(iface.getRefunds(any(), any(), any())).thenReturn(new HistoricalDataResponse()
+                .setData(createHistoricalDataRefunds()));
+        LinkedMultiValueMap<String, String> params = createParams();
+        this.mockMvc.perform(get("/payments-historical-data/refunds")
+                .queryParams(params))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .json("{\"continuationId\":null,\"result\":[{\"id\":\"test\",\"paymentId\":null," +
+                                "\"eventTime\":\"2021-07-29T13:16:18.348795\",\"merchantInfo\":{\"partyId\":\"party\"," +
+                                "\"shopId\":\"shop\"},\"paymentTool\":" +
+                                "\"BankCard(token:null, payment_system:PaymentSystemRef(id:visa), bin:1234, " +
+                                "last_digits:null, bank_name:test)\",\"amount\":123,\"currency\":\"RUB\"," +
+                                "\"provider\":{\"providerId\":\"test\",\"terminalId\":\"1234\",\"country\":\"RUS\"}," +
+                                "\"status\":\"succeeded\",\"error\":{\"errorCode\":null,\"errorReason\":null}," +
+                                "\"clientInfo\":{\"ip\":\"123.123.123.123\",\"fingerprint\":\"finger\"," +
+                                "\"email\":\"email\"},\"payerType\":null}]}"));
+    }
+
+    @Test
+    public void filterInspectResults() throws Exception {
+        when(iface.getFraudResults(any(), any(), any())).thenReturn(new HistoricalDataResponse()
+                .setData(createHistoricalDataInspectResults()));
+        LinkedMultiValueMap<String, String> params = createParams();
+        this.mockMvc.perform(get("/payments-historical-data/inspect-results")
+                .queryParams(params))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .json("{\"continuationId\":null,\"result\":[{\"payment\":{\"id\":\"test\"," +
+                                "\"eventTime\":\"2021-07-29T13:16:18.348795\",\"merchantInfo\":{\"partyId\":\"party\"," +
+                                "\"shopId\":\"shop\"},\"amount\":123,\"currency\":\"RUB\",\"cardToken\":null," +
+                                "\"clientInfo\":{\"ip\":\"123.123.123.123\",\"fingerprint\":\"finger\"," +
+                                "\"email\":\"email\"},\"status\":\"captured\",\"payerType\":null,\"mobile\":null," +
+                                "\"recurrent\":null,\"error\":{\"errorCode\":null,\"errorReason\":null}," +
+                                "\"paymentSystem\":\"visa\",\"paymentCountry\":null,\"paymentTool\":" +
+                                "\"BankCard(token:null, payment_system:PaymentSystemRef(id:visa), bin:1234, " +
+                                "last_digits:null, bank_name:test)\",\"provider\":{\"providerId\":\"test\"," +
+                                "\"terminalId\":\"1234\",\"country\":\"RUS\"}},\"checkedTemplate\":null," +
+                                "\"resultStatus\":\"<ResultStatus accept:Accept()>\",\"ruleChecked\":null," +
+                                "\"notificationsRule\":null}]}"));
+    }
+
+    @Test
+    public void filterFraudPayments() throws Exception {
+        when(iface.getFraudPayments(any(), any(), any())).thenReturn(new HistoricalDataResponse()
+                .setData(createHistoricalDataFraudPaymentInfos()));
+        LinkedMultiValueMap<String, String> params = createParams();
+        this.mockMvc.perform(get("/payments-historical-data/fraud-payments")
+                .queryParams(params))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .json("{\"continuationId\":null,\"result\":[{\"payment\":{\"id\":\"test\"," +
+                                "\"eventTime\":\"2021-07-29T13:16:18.348795\",\"merchantInfo\":{\"partyId\":\"party\"," +
+                                "\"shopId\":\"shop\"},\"amount\":123,\"currency\":\"RUB\",\"cardToken\":null," +
+                                "\"clientInfo\":{\"ip\":\"123.123.123.123\",\"fingerprint\":\"finger\"," +
+                                "\"email\":\"email\"},\"status\":\"captured\",\"payerType\":null,\"mobile\":null," +
+                                "\"recurrent\":null,\"error\":{\"errorCode\":null,\"errorReason\":null}," +
+                                "\"paymentSystem\":\"visa\",\"paymentCountry\":null,\"paymentTool\":" +
+                                "\"BankCard(token:null, payment_system:PaymentSystemRef(id:visa), bin:1234, " +
+                                "last_digits:null, bank_name:test)\",\"provider\":{\"providerId\":\"test\"," +
+                                "\"terminalId\":\"1234\",\"country\":\"RUS\"}},\"type\":\"type_test\"," +
+                                "\"comment\":\"test_comment\"}]}"));
+    }
+
+    @Test
+    public void filterChargebacks() throws Exception {
+        when(iface.getChargebacks(any(), any(), any())).thenReturn(new HistoricalDataResponse()
+                .setData(createHistoricalDataChargebacks()));
+        LinkedMultiValueMap<String, String> params = createParams();
+        this.mockMvc.perform(get("/payments-historical-data/chargebacks")
+                .queryParams(params))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .json("{\"continuationId\":null,\"result\":[{\"id\":\"test\",\"paymentId\":null," +
+                                "\"eventTime\":\"2021-07-29T13:16:18.348795\",\"merchantInfo\":{\"partyId\":\"party\"," +
+                                "\"shopId\":\"shop\"},\"paymentTool\":\"BankCard(token:null, " +
+                                "payment_system:PaymentSystemRef(id:visa), bin:1234, last_digits:null, bank_name:test)\"" +
+                                ",\"amount\":123,\"currency\":\"RUB\",\"provider\":{\"providerId\":\"test\"," +
+                                "\"terminalId\":\"1234\",\"country\":\"RUS\"},\"status\":\"accepted\"," +
+                                "\"category\":\"authorisation\",\"chargebackCode\":\"123\",\"clientInfo\":" +
+                                "{\"ip\":\"123.123.123.123\",\"fingerprint\":\"finger\",\"email\":\"email\"}," +
+                                "\"payerType\":null}]}"));
     }
 
     @NotNull
-    private HistoricalData createHistoricalData() {
-        HistoricalData historicalData = new HistoricalData();
-        BankCard bankCard = new BankCard()
-                .setBankName("test")
-                .setBin("1234")
-                .setPaymentSystem(new PaymentSystemRef()
-                        .setId("visa"));
-        PaymentTool paymentTool = new PaymentTool();
-        paymentTool.setBankCard(bankCard);
-        ReferenceInfo referenceInfo = new ReferenceInfo();
-        referenceInfo.setMerchantInfo(new MerchantInfo().setPartyId("party")
-                .setShopId("shop"));
-        historicalData.setPayments(List.of(
-                new Payment()
-                        .setId("test")
-                        .setMobile(false)
-                        .setEventTime("2021-07-29T13:16:18.348795")
-                        .setPaymentTool(paymentTool)
-                        .setClientInfo(new ClientInfo()
-                                .setEmail("email")
-                                .setFingerprint("finger")
-                                .setIp("123.123.123.123"))
-                        .setCost(new Cash()
-                                .setAmount(123L)
-                                .setCurrency(new CurrencyRef()
-                                        .setSymbolicCode("RUB")))
-                        .setProviderInfo(new ProviderInfo()
-                                .setProviderId("test")
-                                .setCountry("RUS")
-                                .setTerminalId("1234"))
-                        .setReferenceInfo(referenceInfo)
-                        .setStatus(PaymentStatus.captured))
-        );
-        return historicalData;
+    private LinkedMultiValueMap<String, String> createParams() {
+        LinkedMultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("paymentId", "test");
+        params.add("size", "100");
+        params.add("from", "2021-07-27 00:00:00");
+        params.add("to", "2021-07-27 13:28:54");
+        return params;
     }
+
 }
