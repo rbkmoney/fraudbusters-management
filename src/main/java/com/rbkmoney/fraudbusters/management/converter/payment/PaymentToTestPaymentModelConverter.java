6ee01b103fcfd5a45011170201c55b332ebbb64c
k.struzhkin <koztyastruga@gmail.com>
2021-08-23 17:01:45 +0300

    Fix template

--- src/main/java/com/rbkmoney/fraudbusters/management/converter/payment/PaymentToTestPaymentModelConverter.java
+++ src/main/java/com/rbkmoney/fraudbusters/management/converter/payment/PaymentToTestPaymentModelConverter.java
@@ -12,45 +12,48 @@ import org.springframework.stereotype.Component;
 public class PaymentToTestPaymentModelConverter
         implements Converter<com.rbkmoney.damsel.fraudbusters.Payment, TestPaymentModel> {
 
-    public static final String BANK_CARD = "bank_card";
-
     @NonNull
     @Override
     public TestPaymentModel convert(com.rbkmoney.damsel.fraudbusters.Payment payment) {
-        var bankCard = payment.getPaymentTool().getBankCard();
+        TestPaymentModel.TestPaymentModelBuilder builder = TestPaymentModel.builder();
         var referenceInfo = payment.getReferenceInfo();
+        if (referenceInfo != null) {
+            builder.partyId(referenceInfo.getMerchantInfo().getPartyId())
+                    .shopId(referenceInfo.getMerchantInfo().getShopId());
+        }
+        var bankCard = payment.getPaymentTool().getBankCard();
+        if (bankCard != null) {
+            builder.paymentSystem(bankCard.isSetPaymentSystem() ? bankCard.getPaymentSystem().getId() : null)
+                    .paymentCountry(bankCard.isSetIssuerCountry() ? bankCard.getIssuerCountry().name() : null)
+                    .cardToken(bankCard.getToken())
+                    .bin(bankCard.getBin())
+                    .lastDigits(bankCard.getLastDigits());
+        }
         var providerInfo = payment.getProviderInfo();
+        if (providerInfo != null) {
+            builder.terminalId(providerInfo.getTerminalId())
+                    .providerId(providerInfo.getProviderId())
+                    .country(providerInfo.getCountry());
+        }
         var clientInfo = payment.getClientInfo();
+        if (clientInfo != null) {
+            builder.ip(clientInfo.getIp())
+                    .fingerprint(clientInfo.getFingerprint())
+                    .email(clientInfo.getEmail());
+        }
         var error = payment.getError();
-        return TestPaymentModel.builder()
+        if (error != null) {
+            builder.errorCode(error.getErrorCode())
+                    .errorReason(error.getErrorReason());
+        }
+        return builder
                 .currency(payment.getCost().getCurrency().getSymbolicCode())
                 .amount(payment.getCost().getAmount())
                 .eventTime(payment.getEventTime())
                 .paymentId(payment.getId())
                 .status(payment.getStatus().name())
-                .partyId(referenceInfo != null ? referenceInfo.getMerchantInfo().getPartyId() : null)
-                .shopId(referenceInfo != null ? referenceInfo.getMerchantInfo().getShopId() : null)
-                .terminalId(providerInfo != null ? providerInfo.getTerminalId() : null)
-                .providerId(providerInfo != null ? providerInfo.getProviderId() : null)
-                .country(providerInfo != null ? providerInfo.getCountry() : null)
-                .paymentSystem(bankCard != null && bankCard.isSetPaymentSystem()
-                        ? bankCard.getPaymentSystem().getId()
-                        : null)
-                .paymentCountry(bankCard != null && bankCard.isSetIssuerCountry()
-                        ? bankCard.getIssuerCountry().name()
-                        : null)
-                .cardToken(bankCard != null ? bankCard.getToken() : null)
-                .bin(bankCard != null ? bankCard.getBin() : null)
-                .lastDigits(bankCard != null ? bankCard.getLastDigits() : null)
-                .paymentTool(payment.isSetPaymentTool()
-                        ? payment.getPaymentTool().getFieldValue().toString()
-                        : null)
-                .ip(clientInfo != null ? clientInfo.getIp() : null)
-                .fingerprint(clientInfo != null ? clientInfo.getFingerprint() : null)
-                .email(clientInfo != null ? clientInfo.getEmail() : null)
+                .paymentTool(payment.isSetPaymentTool() ? payment.getPaymentTool().getFieldValue().toString() : null)
                 .payerType(payment.getPayerType().name())
-                .errorCode(error != null ? error.getErrorCode() : null)
-                .errorReason(error != null ? error.getErrorReason() : null)
                 .build();
     }
 }
