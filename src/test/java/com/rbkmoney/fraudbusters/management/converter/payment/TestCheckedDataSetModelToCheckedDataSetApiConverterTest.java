a2b11e4e85d568122f52756619c52040adf00f6c
k.struzhkin <koztyastruga@gmail.com>
2021-08-25 16:28:46 +0300

    Fix date

--- src/test/java/com/rbkmoney/fraudbusters/management/converter/payment/TestCheckedDataSetModelToCheckedDataSetApiConverterTest.java
+++ src/test/java/com/rbkmoney/fraudbusters/management/converter/payment/TestCheckedDataSetModelToCheckedDataSetApiConverterTest.java
@@ -7,19 +7,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {TestCheckedPaymentModelToCheckedDataSetRowConverter.class,
         TestCheckedDataSetModelToCheckedDataSetApiConverter.class,
         TestPaymentModelToPaymentApiConverter.class})
 public class TestCheckedDataSetModelToCheckedDataSetApiConverterTest {
 
+    public static final String TEST = "TEST";
+    public static final String TEST_TEMPLATE = "test_template";
+    public static final String TEST_DATA_SET_ID = "1";
     @Autowired
     TestCheckedDataSetModelToCheckedDataSetApiConverter testCheckedDataSetModelToCheckedDataSetApiConverter;
 
     @Test
     public void testConvert() {
-        TestCheckedDataSetModel testDataSetModel = new TestCheckedDataSetModel();
+        TestCheckedDataSetModel testDataSetModel = TestCheckedDataSetModel.builder()
+                .initiator(TEST)
+                .testDataSetId(1L)
+                .shopId(TEST)
+                .partyId(TEST)
+                .id(2L)
+                .createdAt(LocalDateTime.now())
+                .template(TEST_TEMPLATE)
+                .testCheckedPaymentModels(List.of())
+                .build();
         var checkedDataSet = testCheckedDataSetModelToCheckedDataSetApiConverter.convert(testDataSetModel);
+
+        assertNotNull(checkedDataSet);
+        assertEquals(TEST_DATA_SET_ID, checkedDataSet.getTestDataSetId());
+        assertEquals(TEST, checkedDataSet.getInitiator());
+        assertEquals(TEST_TEMPLATE, checkedDataSet.getTemplate());
+        assertTrue(checkedDataSet.getRows().isEmpty());
     }
 
 }
