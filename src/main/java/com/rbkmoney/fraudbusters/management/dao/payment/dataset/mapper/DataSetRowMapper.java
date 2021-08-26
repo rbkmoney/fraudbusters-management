74a6466f43957985e5ac907fb7c6cd81a4a83f06
k.struzhkin <koztyastruga@gmail.com>
2021-08-26 14:49:01 +0300

    Refactor

--- src/main/java/com/rbkmoney/fraudbusters/management/dao/payment/dataset/mapper/DataSetRowMapper.java
+++ src/main/java/com/rbkmoney/fraudbusters/management/dao/payment/dataset/mapper/DataSetRowMapper.java
@@ -1,17 +1,13 @@
 package com.rbkmoney.fraudbusters.management.dao.payment.dataset.mapper;
 
 import com.rbkmoney.fraudbusters.management.domain.payment.TestCheckedDataSetModel;
-import com.rbkmoney.fraudbusters.management.domain.payment.TestCheckedPaymentModel;
-import com.rbkmoney.fraudbusters.management.domain.payment.TestPaymentModel;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 
-import static com.rbkmoney.fraudbusters.management.domain.Tables.*;
+import static com.rbkmoney.fraudbusters.management.domain.Tables.TEST_DATA_SET;
 import static com.rbkmoney.fraudbusters.management.domain.Tables.TEST_DATA_SET_CHECKING_RESULT;
 
 public class DataSetRowMapper implements RowMapper<TestCheckedDataSetModel> {
@@ -22,8 +18,8 @@ public class DataSetRowMapper implements RowMapper<TestCheckedDataSetModel> {
     public TestCheckedDataSetModel mapRow(ResultSet resultSet, int i) throws SQLException {
         return TestCheckedDataSetModel.builder()
                 .template(resultSet.getString(TEST_DATA_SET_CHECKING_RESULT.TEMPLATE.getName()))
-                .checkingTimestamp(
-                        resultSet.getObject(TEST_DATA_SET_CHECKING_RESULT.CHECKING_TIMESTAMP.getName(), LocalDateTime.class))
+                .checkingTimestamp(resultSet.getObject(TEST_DATA_SET_CHECKING_RESULT.CHECKING_TIMESTAMP.getName(),
+                                LocalDateTime.class))
                 .createdAt(resultSet.getObject(TEST_DATA_SET_CHECKING_RESULT.CREATED_AT.getName(), LocalDateTime.class))
                 .partyId(resultSet.getString(TEST_DATA_SET_CHECKING_RESULT.PARTY_ID.getName()))
                 .shopId(resultSet.getString(TEST_DATA_SET_CHECKING_RESULT.SHOP_ID.getName()))
@@ -32,5 +28,6 @@ public class DataSetRowMapper implements RowMapper<TestCheckedDataSetModel> {
                 .initiator(resultSet.getString(TEST_DATA_SET_CHECKING_RESULT.INITIATOR.getName()))
                 .build();
     }
+
 
 }
